583e173fca976896cadf0e35203ed128082d8abd
Brayan Sanchez <brayan_sanchez99@hotmail.es>
2023-07-08 16:04:24 -0600

    Completing the CRUD of the library in the backend

--- controllers/author.js
+++ controllers/author.js
@@ -7,10 +7,80 @@ exports.createAuthor = async (req, res, next) => {
     const authorData = await Author.create(body);
 
     res.status(200).json({
-      status: 200,
       data: authorData,
+      message: "Autor creado",
     });
   } catch (error) {
-    res.status(400).json({ status: 400, msg: error });
+    res.status(400).json({
+      error: true,
+      message: "Error al crear el autor",
+    });
+  }
+};
+
+exports.getAuthors = async (req, res, next) => {
+  try {
+    const authorList = await Author.find();
+
+    res.status(200).json(authorList);
+  } catch (error) {
+    res
+      .status(400)
+      .json({ error: true, message: "Error al traer los autores" });
+  }
+};
+
+exports.getAuthorById = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const author = await Author.findById(id);
+
+    res.status(200).json(author);
+  } catch (error) {
+    res
+      .status(400)
+      .json({ error: true, message: "Error al obtener a un autor por id" });
+  }
+};
+
+exports.updateAuthor = async (req, res, next) => {
+  const { id } = req.params;
+  const { body } = req;
+
+  try {
+    const author = await Author.findByIdAndUpdate(id, body);
+
+    if (!author) {
+      return res
+        .status(400)
+        .json({ error: true, message: "Autor no encontrado" });
+    }
+
+    res.status(200).json({ data: author, msg: "Autor ya actualizado" });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ error: true, message: "Error al actualizar al autor" });
+  }
+};
+
+exports.deleteAuthor = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const author = await Author.findByIdAndDelete(id);
+
+    if (!author) {
+      return res
+        .status(400)
+        .json({ error: true, message: "Autor no encontrado" });
+    }
+
+    res.status(200).json({ msg: "Autor eliminado" });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ error: true, message: "Error al eliminar al autor" });
   }
 };
--- routes/author.js
+++ routes/author.js
@@ -2,8 +2,15 @@ const Router = require("express");
 const express = require("express");
 const api = express.Router();
 
-const { createAuthor } = require("../controllers/author.js");
+const {
+  createAuthor,
+  getAuthors,
+  getAuthorById,
+  updateAuthor,
+  deleteAuthor,
+} = require("../controllers/author.js");
 
-api.route("/").post(createAuthor);
+api.route("/").post(createAuthor).get(getAuthors);
+api.route("/:id").get(getAuthorById).put(updateAuthor).delete(deleteAuthor);
 
 module.exports = api;
